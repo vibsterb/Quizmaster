8720ad18e1f6a8e8d42f4f53eb0ca29c4eac9c41
Vibeke Braaten <vibeke.braaten@gmail.com>
2019-11-17 21:52:11 +0100

    running server

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const db = require("./js/db.js");
-const user = require("./js/user.js");
+/*const db = require("./js/db.js");*/
+/*const user = require("./js/user.js");*/
 
 
 app.set('port', (process.env.PORT ||8080));
